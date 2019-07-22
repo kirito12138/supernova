4da80f95f039250c0ce8d69c4b3ea92594bbed0d
williambao(鲍屹伟) <williambao@tencent.com>
2019-07-22 18:28:12 +0800

    complete drag and swap

---
+++ Assets/boxScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxScript : MonoBehaviour
+{
+    public bool ifOccupied = false;
+    public GameObject crtPiece = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/mouseDrag.cs
+++ Assets/mouseDrag.cs
@@ -4,52 +4,243 @@ using UnityEngine;
 
 public class mouseDrag : MonoBehaviour
 {
-    public bool isMouseDown = false;
+    //private bool isMouseDown = false;
+    //==========================用于drag=====================================
     private Vector3 lastMousePosition = Vector3.zero;
     private Vector3 offset;
     private Vector3 mp = Vector3.zero;
-    private string hitTag;
-    private Transform tf;
+    //-----------------------------------------------------------------------
+
+    //=====================用于back，move2等=================================
+    private Vector3 orgPosition;
+    //private bool ifClose = false;
+    //private float crtSpeed = 0;
+    private float beginTime;
+    public float smoothTime = 0.08f;
+    public float attachDst = 1.0f;
+    private GameObject crtBox = null;
+    private Vector3 moveDst;
+    private Vector3 crtPosition;
+    private Vector3 direction;
+    private GameObject[] boxes = new GameObject[6];
+
+    
+
+    enum states
+    {
+        idle,
+        drag,
+        move2,
+        back,
+        inBox,
+        reset
+    };
+
+    private states state;
+
+    private void Start()
+    {
+        state = states.idle;
+        boxes = GameObject.FindGameObjectsWithTag("EmptyBox");
+        crtPosition = this.transform.position;
+        orgPosition = this.transform.position;
+
+    }
+
     private void Update()
     {
 
         if (Input.GetMouseButtonDown(0))
         {
-            isMouseDown = true;
-            lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity);
-            hitTag = hit.collider.tag;
-            tf = hit.collider.transform;
-            mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //print("down");
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            isMouseDown = false;
-            hitTag = "";
-            tf = null;
-            //print("up");
-        }
 
-        if (isMouseDown)
-        {
-            
-            if (tf != null && hitTag == "PuzzlePiece")
+            if (hit!=null && hit.collider.gameObject == this.gameObject)
             {
-                MoveCube();
-
+                toDrag(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             }
+
+        }
+        if (state == states.drag && Input.GetMouseButtonUp(0))
+        {
+            bool inBox = false;
+            foreach (GameObject item in boxes)
+            {
+                Vector2 boxPst = new Vector2(item.transform.position.x, item.transform.position.y);
+                Vector2 crtPst = new Vector2(this.transform.position.x, this.transform.position.y);
+                if (Vector2.Distance(boxPst, crtPst) <= attachDst)
+                {
+                    toMove2(item, false);
+                    inBox = true;
+                    break;
+                }
+            }
+            if (inBox == false)
+            {
+                toBack();
+            }
+            
+        }
+
+        if (state == states.drag)
+        {
+            MoveCube();
+        }
+        else if (state == states.back)
+        {
+            MoveBack();
+            Vector2 crtP = new Vector2(this.transform.position.x, this.transform.position.y);
+            Vector2 dstP = new Vector2(moveDst.x, moveDst.y);
+            if (Vector2.Distance(crtP,dstP)<0.01)
+            {
+                this.transform.position = moveDst;
+                toIdle();
+            }
+        }
+        else if (state == states.move2)
+        {
+            MoveBack();
+            Vector2 crtP = new Vector2(this.transform.position.x, this.transform.position.y);
+            Vector2 dstP = new Vector2(moveDst.x, moveDst.y);
+            if (Vector2.Distance(crtP, dstP) < 0.01)
+            {
+                this.transform.position = moveDst;
+                toIdle();
+            }
+        }
+        else if (state == states.reset)
+        {
+            MoveBack();
         }
     }
 
-    void MoveCube()
+    private void toIdle()
+    {
+        print("to state Idle");
+        state = states.idle;
+    }
+
+    private void toDrag(Vector3 mPst)
+    {
+        print("to state Drag");
+        mp = mPst;
+        lastMousePosition = mPst;
+        state = states.drag;
+        moveDst = crtPosition;
+        moveDst.z = 0;
+    }
+
+    private void toBack()
+    {
+        print("to state Back");
+        state = states.back;
+        moveDst = orgPosition;
+        
+        if (crtBox != null)
+        {
+            crtBox.GetComponent<boxScript>().crtPiece = null;
+        }
+        crtBox = null;
+        //var str = string.Format("原始位置：{0},  方向：{1}", crtPosition,direction);
+        //print(str);
+    }
+
+    private void toReset()      //只负责清楚自身状态并回到等待区，不负责box状态更改。
+    {
+        print("to state Reset");
+        crtBox = null;
+        moveDst = orgPosition;
+    }
+
+    private void toMove2(GameObject dst,bool ifSwap)    //ifSwap:是否为被动交换位置
+    {
+        print("to state Move2");
+        moveDst = dst.transform.position;
+        moveDst.z = 0;
+        state = states.move2;
+        if (ifSwap==false && dst.GetComponent<boxScript>().crtPiece!=null)
+        {
+            if (crtBox == null)
+            {
+                dst.GetComponent<boxScript>().crtPiece.GetComponent<mouseDrag>().move2(null, 0);
+            }
+            else
+            {
+                dst.GetComponent<boxScript>().crtPiece.GetComponent<mouseDrag>().move2(crtBox, 1);
+            }
+            
+        }
+        crtBox = dst;
+        dst.GetComponent<boxScript>().crtPiece = this.gameObject;
+    }
+    private void MoveCube()
     {
         mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         offset = mp - lastMousePosition;
         offset.z = 0;
         // Debug.Log("offset:" + offset);
-        tf.position += offset;
+        this.transform.position += offset;
         lastMousePosition = mp;
         //Debug.Log(offset);
+    }
+
+    private void MoveBack()
+    {
+        Vector3 z = Vector3.zero;
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, moveDst, ref z, smoothTime);
+        
+
+    }
+
+    
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰撞！");
+        if (collision.tag == "EmptyBox")
+        {
+            if (collision.GetComponent<boxScript>().crtPiece == null)
+            {
+                collision.GetComponent<boxScript>().crtPiece = this.gameObject;
+                moveDst = collision.transform.position;
+            }
+            else
+            {
+
+            }
+            crtBox = collision.gameObject;
+
+            moveDst = collision.transform.position;
+            //collision.GetComponent<boxScript>().crtPiece = this.gameObject;
+            //crtBox = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        print("出去了");
+        if (collision.tag == "EmptyBox")
+        {
+            //crtBox = null;
+            moveDst = crtPosition;
+        }
+    }*/
+
+    public bool move2(GameObject dst, int mode)   //mode: 0:回到最下方等待区域   1:移动到dst所在位置
+    {
+
+        if (mode == 0)
+        {
+            if (crtBox!=null)
+            {
+                crtBox.GetComponent<boxScript>().crtPiece = null;
+            }
+            toBack();
+        }
+        else
+        {
+            toMove2(dst, true);
+        }
+        return true;
+
     }
 }
