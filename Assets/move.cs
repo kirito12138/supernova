df6a03ba7a325f571e5595b95c448df032a3e4ee
williambao(鲍屹伟) <williambao@tencent.com>
2019-07-27 17:59:36 +0800

    finish move trick, figured how to use ps (roughly), drawed some figures

---
+++ Assets/move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public GameObject image;
+    public float offsetX;
+    private int direction;
+    private Vector3 moveDst;
+    private bool ifMove;
+    private Vector3 z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = 1;
+        ifMove = false;
+        z = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ifMove)
+        {
+            image.transform.position = Vector3.SmoothDamp(image.transform.position, moveDst, ref z, 0.8f);
+            if (Mathf.Abs(image.transform.position.x - moveDst.x)<=0.1)
+            {
+                image.transform.position = moveDst;
+                ifMove = false;
+            }
+        }
+    }
+
+    public void Move()
+    {
+        this.GetComponent<SpriteRenderer>().flipX = !this.GetComponent<SpriteRenderer>().flipX;
+        direction *= -1;
+        moveDst = image.transform.position;
+        moveDst.x += (direction * offsetX);
+        ifMove = true;
+    }
+}
